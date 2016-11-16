26adb8b7898a26a19a05573acd7bfe530df824ae
CleverFan <377109151@qq.com>
2016-11-16 15:36:17 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// router.get('/vuedemo', function(req, res, next) {
+//   res.writeHead(302,{
+//     'Location':'www.improvecfan.cn:8089'
+//   });
+//   res.end();
+// });
+//
+//
+// router.get('/blog', function(req, res, next) {
+//   res.writeHead(302,{
+//     'Location':'http://blog.improvecfan.cn'
+//   });
+//   res.end();
+// });
+
+module.exports = router;
