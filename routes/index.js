e48ca368350e0faab87eb8285e7812ad3e9826e8
CleverFan <377109151@qq.com>
2016-11-16 15:44:54 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/vuedemo', function(req, res, next) {
-//   res.writeHead(302,{
-//     'Location':'www.improvecfan.cn:8089'
-//   });
-//   res.end();
-// });
-//
-//
-// router.get('/blog', function(req, res, next) {
-//   res.writeHead(302,{
-//     'Location':'http://blog.improvecfan.cn'
-//   });
-//   res.end();
-// });
+router.get('/vuedemo', function(req, res, next) {
+  res.writeHead(302,{
+    'Location':'www.improvecfan.cn:8089'
+  });
+  res.end();
+});
+
+
+router.get('/blog', function(req, res, next) {
+  res.writeHead(302,{
+    'Location':'http://blog.improvecfan.cn'
+  });
+  res.end();
+});
 
 module.exports = router;
